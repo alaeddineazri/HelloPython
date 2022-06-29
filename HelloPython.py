afd28a871a81c20490c9ebb3e9a14a8289321a0d
AlaeddineAzri <alaeddinazri@gmail.com>
2022-06-29 21:13:01 +0100

    numpy

--- HelloPython.py
+++ HelloPython.py
@@ -252,3 +252,45 @@ dist = radians(12) * r
 
 # Print out dist
 print(dist)
+
+
+# ! NumPy
+# ? why we use NumPy
+# consume less memory
+# easy to manipulate
+# faster the python list
+# Support element wise operations
+# elements are stored contiguous
+# NumPy in github => https://github.com/numpy/numpy 
+# pip3 install numpy => install numpy 
+
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np 
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+print ( np_baseball)
+
+
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball= np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+print (np_baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
